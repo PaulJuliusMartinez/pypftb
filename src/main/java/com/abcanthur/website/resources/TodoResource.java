ff7c73447d64de3a5c433be160e4d7a671a8bfb0
Peter John Martinez <peterjohn.martinez@gmail.com>
2017-04-11 16:31:20 -0700

    almost to cru,d

--- src/main/java/com/abcanthur/website/resources/TodoResource.java
+++ src/main/java/com/abcanthur/website/resources/TodoResource.java
@@ -1,5 +1,6 @@
 package com.abcanthur.website.resources;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.ws.rs.FormParam;
@@ -16,6 +17,7 @@ import javax.ws.rs.core.MediaType;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.exception.DataAccessException;
+import org.jooq.impl.DSL;
 import org.mindrot.jbcrypt.BCrypt;
 
 import static com.abcanthur.website.codegen.Tables.*;
@@ -32,11 +34,10 @@ import com.abcanthur.website.codegen.tables.records.UserRecord;
 //parent_id			integer REFERENCES todos (id) ON DELETE cascade,
 //user_id 			integer NOT NULL REFERENCES users (id) ON DELETE cascade
 
-@Path("/todo")
+@Path("/todos")
 public class TodoResource {
 	
 	@POST
-	@Path("/create")
 	@Produces(MediaType.APPLICATION_JSON)
 	public TodoPojo create(
 			@FormParam("body") Optional<String> body,
@@ -56,25 +57,57 @@ public class TodoResource {
 		} catch (DataAccessException e) {
 			throw new WebApplicationException("Unknown error", 400);
 		}
-		TodoPojo todo = database.selectFrom(TODOS)
-				.where(TODOS.ID.equal(record.getValue(TODOS.ID)))
-				.fetchOne()
-				.into(TodoPojo.class);
-		return todo;
+		return getTodoPojoById(database, record.getValue(TODOS.ID));
 	}
 	
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public TodoPojo getTodoById(
+	public TodoPojo returnTodoById(
 		@Context DSLContext database,
 		@PathParam("id") Integer id
 	) {
-		TodoPojo todoPojo = database
+		return getTodoPojoById(database, id);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<TodoPojo> getTodoByUser(
+			@Context UserRecord user,
+			@Context DSLContext database
+			) {
+		return database
+				.selectFrom(TODOS)
+				.where(TODOS.USER_ID.equal(user.getId()))
+				.fetch()
+				.into(TodoPojo.class);
+	}
+	
+	@POST
+	@Path("/{id}/complete")
+	@Produces(MediaType.APPLICATION_JSON)
+	public TodoPojo completeTodo(
+		@Context DSLContext database,
+		@PathParam("id") Integer id
+	) {
+		database
+			.update(TODOS)
+			.set(TODOS.COMPLETED, true)
+			.set(TODOS.DATE_COMPLETED, DSL.currentTimestamp())
+			.where(TODOS.ID.equal(id), TODOS.COMPLETED.equal(false))
+			.execute();
+		return getTodoPojoById(database, id);
+	}
+	
+	private TodoPojo getTodoPojoById(DSLContext database, Integer id) {
+		return getTodoRecordById(database, id).into(TodoPojo.class);
+	}
+	
+	private TodoRecord getTodoRecordById(DSLContext database, Integer id) {
+		TodoRecord todoRecord = database
 				.selectFrom(TODOS)
 				.where(TODOS.ID.equal(id))
-				.fetchOne()
-				.into(TodoPojo.class);
-		return todoPojo;
-	}	
+				.fetchOne();
+		return todoRecord;
+	}
 }
