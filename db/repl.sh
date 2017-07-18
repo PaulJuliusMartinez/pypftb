6abafc08a647495ef7084de05cfaf6d94f24c772
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2017-07-17 19:34:35 -0700

    Change DB to pypftb.

--- db/repl.sh
+++ db/repl.sh
@@ -1,1 +1,1 @@
-psql postgres://`grep User secrets.yml | cut -d " " -f 2`:`grep Password secrets.yml | cut -d " " -f 2`@pauljuliusmartinez-test.cllhowwyaqxl.us-west-1.rds.amazonaws.com/postgres
+psql postgres://`grep User secrets.yml | cut -d " " -f 2`:`grep Password secrets.yml | cut -d " " -f 2`@`grep Url secrets.yml | cut -d "/" -f 3,4`
