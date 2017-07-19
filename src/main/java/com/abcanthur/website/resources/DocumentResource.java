e7620957d63fb2d4f49b0200bf9e79eb0fcad83f
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2017-07-18 22:06:23 -0700

    Render list view with links to previous and next.

--- src/main/java/com/abcanthur/website/resources/DocumentResource.java
+++ src/main/java/com/abcanthur/website/resources/DocumentResource.java
@@ -1,19 +1,31 @@
 package com.abcanthur.website.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import org.jooq.DSLContext;
+import org.jooq.Record;
 
+import static com.abcanthur.website.codegen.Tables.*;
+
+import com.abcanthur.website.codegen.tables.pojos.PledgePojo;
 import com.abcanthur.website.views.HomeView;
 import com.abcanthur.website.views.ListView;
 import com.abcanthur.website.views.MethodsView;
 
 @Path("/")
 public class DocumentResource {
+	
+	public static int DEFAULT_COUNT = 50;
+	public static int MAX_COUNT = 100;
 	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
@@ -24,8 +36,33 @@ public class DocumentResource {
 	@GET
 	@Path("/list")
 	@Produces(MediaType.TEXT_HTML)
-	public ListView list(@Context DSLContext database) {
-		return new ListView("This is where you can see all the pee-ers");
+	public ListView list(
+		@Context DSLContext database,
+		@QueryParam("count") Optional<Integer> countParam,
+		@QueryParam("offset") Optional<Integer> offsetParam
+	) {
+		int count = countParam.orElse(DEFAULT_COUNT);
+		int offset = offsetParam.orElse(0);
+		
+		// Safeguards against manually-edited queries.
+		if (count < 0) count = DEFAULT_COUNT;
+		if (count > MAX_COUNT) count = MAX_COUNT;
+		if (offset < 0) offset = 0;
+		
+		System.out.println(count);
+		System.out.println(offset);
+		
+		List<PledgePojo> pledges = database
+			.selectFrom(PLEDGES)
+			.limit(count)
+			.offset(offset)
+			.fetchInto(PledgePojo.class);
+		
+		Record record = database.selectCount().from(PLEDGES).fetchOne();
+		int total = (Integer) record.get(0);
+		System.out.println("Total: " + total);
+			
+		return new ListView(pledges, count, offset, total);
 	}
 	
 	@GET
--- src/main/java/com/abcanthur/website/views/ListView.java
+++ src/main/java/com/abcanthur/website/views/ListView.java
@@ -1,14 +1,50 @@
 package com.abcanthur.website.views;
 
+import java.util.List;
+
+import com.abcanthur.website.codegen.tables.pojos.PledgePojo;
+
 import io.dropwizard.views.View;
 
 public class ListView extends View {
-	private String testStr;
-	public ListView(String s) {
+	
+	private List<PledgePojo> pledges;
+	private int count;
+	private int offset;
+	private int total;
+	
+	public ListView(List<PledgePojo> pledges, int count, int offset, int total) {
 		super("list.mustache");
-		this.testStr = s;
+		this.pledges = pledges;
+		this.count = count;
+		this.offset = offset;
+		this.total = total;
 	}
-	public String getStr() {
-		return this.testStr;
+	
+	public List<PledgePojo> getPledges() {
+		return this.pledges;
 	}
+	
+	public int getCount() {
+		return this.count;
+	}
+	
+	public int getOffset() {
+		return this.offset;
+	}
+	
+	public String getLinkToEarlierPledges() {
+		if (offset == 0) return null;
+		
+		int newOffset = offset - count;
+		if (newOffset < 0) newOffset = 0;
+		
+		return "/list?count=" + count + "&offset=" + newOffset; 
+	}
+	
+	public String getLinkToLaterPledges() {
+		if (offset + count >= total) return null;
+		return "/list?count=" + count + "&offset=" + (offset + count);		
+	}
+	
 }
