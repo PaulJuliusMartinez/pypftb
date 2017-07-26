4ca2881f27c498e3231447d60806f008d10604e3
Paul Julius Martinez <pauljuliusmartinez@gmail.com>
2017-07-25 23:12:12 -0700

    Basic styles for list page.

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -1,1 +1,36 @@
-console.log('hello');
+window.onload = () => {
+  let pledgeButton = document.getElementById("pledge-button");
+  pledgeButton.onclick = handlePledgeSubmit;
+};
+
+let handlePledgeSubmit = e => {
+  // Don't let form submit.
+  e.preventDefault();
+
+  let pledgeForm = document.getElementById("pledge-form");
+  let formData = new FormData(pledgeForm);
+
+  let urlEncodedData = "";
+
+  for (let key of formData.keys()) {
+    if (urlEncodedData.length !== 0) urlEncodedData += '&';
+    urlEncodedData += key;
+    urlEncodedData += '=';
+    urlEncodedData += encodeURIComponent(formData.get(key)).replace(/%20/g, '+');
+  }
+
+  let req = new XMLHttpRequest();
+  let url = "/api/pledge";
+
+  req.open("POST", url, true);
+
+  req.onreadystatechange = () => {
+    if (req.readyState === 4 && req.status === 400) {
+      let pledgeError = document.getElementById("pledge-error");
+      pledgeError.innerHTML = JSON.parse(req.responseText).message;
+    }
+  };
+
+  req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  req.send(urlEncodedData);
+};
